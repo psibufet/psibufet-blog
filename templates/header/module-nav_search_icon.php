b28505588e47b0d18cc5cf78373ca7c4aab25645
codestickdev <piotrdevv@gmail.com>
2022-02-07 12:06:45 +0100

    Menu search form

--- templates/header/module-nav_search_icon.php
+++ templates/header/module-nav_search_icon.php
@@ -2,4 +2,7 @@
 	<a href="#" id="ruby-nav-search" data-mfp-src="#ruby-banner-search-form" data-effect="mpf-ruby-effect" title="<?php esc_attr_e('search', 'look'); ?>" class="banner-search-icon">
 		<i class="fa-rb fa-search"></i>
 	</a>
+	<div class="searchBar">
+		<?php echo get_search_form(); ?>
+	</div>
 </div>
