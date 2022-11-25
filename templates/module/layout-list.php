b96cfbcb61a743eb06db88f506e33ed13b4a492e
codestickdev <piotrdevv@gmail.com>
2022-11-25 15:24:31 +0100

    reading time, publish date - posts listing

--- templates/module/layout-list.php
+++ templates/module/layout-list.php
@@ -16,7 +16,12 @@ if ( is_sticky() ) {
 if ( ! empty( $look_ruby_smooth_display_enable ) ) {
 	$look_ruby_classes[] = 'ruby-animated-image ' . esc_attr( $look_ruby_smooth_display_style );
 }
-$look_ruby_classes = implode( ' ', $look_ruby_classes ); ?>
+$look_ruby_classes = implode( ' ', $look_ruby_classes );
+$readingTime = get_post_meta(get_the_ID())['_yoast_wpseo_estimated-reading-time-minutes'][0];
+$createDate = get_the_date();
+$createDate_mobile = get_the_date('d.m.Y');
+$updateDate = get_the_modified_date();
+$updateDate_mobile = get_the_modified_date('d.m.Y'); ?>
 <article <?php look_ruby_markup_article(); post_class( esc_attr( $look_ruby_classes ) ); ?>>
 	<?php
 	$look_ruby_featured = get_the_post_thumbnail( get_the_ID(), 'look_ruby_360_250' );
@@ -47,9 +52,26 @@ $look_ruby_classes = implode( ' ', $look_ruby_classes ); ?>
 					<?php if ( ! empty( $look_ruby_post_share_bar_list ) ) : ?>
 						<?php look_ruby_template_part::post_share_bar(); ?>
 					<?php endif; ?>
+					<div class="readingTime"><p>Przeczytasz w <?php echo $readingTime; ?> min.</p></div>
+					<?php if($createDate !== $updateDate): ?>
+						<div class="publishDate"><p><?php echo $updateDate; ?></p></div>
+					<?php else: ?>
+						<div class="publishDate"><p><?php echo $createDate; ?></p></div>
+					<?php endif; ?>
 				</footer>
 				<?php get_template_part( 'templates/meta/el', 'meta_footer' ); ?>
 			</div>
 		</div>
+	</div>
+	<div class="footerMobile">
+		<?php if ( ! empty( $look_ruby_post_share_bar_list ) ) : ?>
+			<?php look_ruby_template_part::post_share_bar(); ?>
+		<?php endif; ?>
+		<div class="readingTime"><p><span>Przeczytasz w </span><?php echo $readingTime; ?> min.</p></div>
+		<?php if($createDate !== $updateDate): ?>
+			<div class="publishDate"><p class="desktop"><?php echo $updateDate; ?></p><p class="mobile"><?php echo $updateDate_mobile; ?></p></div>
+		<?php else: ?>
+			<div class="publishDate"><p class="desktop"><?php echo $createDate; ?></p><p class="mobile"><?php echo $createDate_mobile; ?></p></div>
+		<?php endif; ?>
 	</div>
 </article>
